267f0c42882366bbdb15945339017580d4b7cb3c
kgreav <kurt.greaves@zepben.com>
2020-12-23 09:00:22 +1100

    Add ExceptionInterceptor for handling and logging server side errors (#2)
    * Add ExceptionInterceptor for handling and logging server side errors
    
    Signed-off-by: Kurt Greaves <kurt.greaves@zepben.com>

--- src/test/kotlin/com/zepben/auth/grpc/AuthInterceptorTest.kt
+++ src/test/kotlin/com/zepben/auth/grpc/AuthInterceptorTest.kt
@@ -9,9 +9,8 @@
 package com.zepben.auth.grpc
 
 import com.zepben.auth.JWTAuthenticator
+import com.zepben.evolve.conn.grpc.runServerCall
 import com.zepben.testutils.auth.MockJwksUrlProvider
-import com.zepben.testutils.auth.MockServerCall
-import com.zepben.testutils.auth.MockServerCallHandler
 import com.zepben.testutils.auth.TOKEN
 import io.grpc.Metadata
 import io.grpc.Status
@@ -25,41 +24,41 @@ class AuthInterceptorTest {
 
     @Test
     fun testIntercept() {
-        var ta = JWTAuthenticator("https://fake-aud/", "issuer", MockJwksUrlProvider())
+        val ta = JWTAuthenticator("https://fake-aud/", "issuer", MockJwksUrlProvider())
         val requiredScopes = mapOf(
             "zepben.protobuf.np.NetworkProducer" to write_network_scope
         )
         val authInterceptor = AuthInterceptor(ta, requiredScopes)
-        var sc = MockServerCall<Int, Int>({ status, _ ->
-            assertThat(status!!.code, equalTo(Status.UNAUTHENTICATED.code))
-            assertThat(status.description, equalTo("Authorization token is missing"))
-        })
-        authInterceptor.interceptCall(sc, Metadata(), null)
 
-        val mdNotBearer = Metadata().apply { put(AUTHORIZATION_METADATA_KEY, "NotBearer ayyyyy") }
-        sc = MockServerCall({ status, _ ->
-            assertThat(status!!.code, equalTo(Status.UNAUTHENTICATED.code))
-            assertThat(status.description, equalTo("Unknown authorization type"))
-        })
-        authInterceptor.interceptCall(sc, mdNotBearer, null)
+        runServerCall(authInterceptor,
+            { status, _ ->
+                assertThat(status!!.code, equalTo(Status.UNAUTHENTICATED.code))
+                assertThat(status.description, equalTo("Authorization token is missing"))
+            },
+            null
+        )
 
-        val mdWithBearer = Metadata().apply { put(AUTHORIZATION_METADATA_KEY, "Bearer $TOKEN") }
-        sc = MockServerCall({ _, _ -> })
-        var callWasMade = false
-        val sch = MockServerCallHandler<Int, Int> { _, metadata ->
-            // not really important assert - just to make sure no one balls'd up the test
-            assertThat("Metadata had bearer token.", metadata.containsKey(AUTHORIZATION_METADATA_KEY))
-            callWasMade = true
-        }
-        authInterceptor.interceptCall(sc, mdWithBearer, sch)
-        assertThat(callWasMade, equalTo(true))
+        runServerCall(authInterceptor,
+            { status, _ ->
+                assertThat(status!!.code, equalTo(Status.UNAUTHENTICATED.code))
+                assertThat(status.description, equalTo("Unknown authorization type"))
+            }, null, metadata = Metadata().apply { put(AUTHORIZATION_METADATA_KEY, "NotBearer ayyyyy") }
+        )
 
-        callWasMade = false
-        val mdWithBadBearer = Metadata().apply { put(AUTHORIZATION_METADATA_KEY, "Bearer aoeu") }
-        sc = MockServerCall({ status, _ ->
-            assertThat(status!!.code, equalTo(Status.UNAUTHENTICATED.code))
-        })
-        authInterceptor.interceptCall(sc, mdWithBadBearer, sch)
-        assertThat(callWasMade, equalTo(false))
+        runServerCall(authInterceptor,
+            { _, _ -> assert(true)},
+            { sc, metadata -> assertThat("Metadata had bearer token.", metadata.containsKey(AUTHORIZATION_METADATA_KEY))
+                sc.close(null, null)  // Close for some reason doesn't occur at any point on a successful auth, so we call it ourselves.
+            },
+            metadata = Metadata().apply { put(AUTHORIZATION_METADATA_KEY, "Bearer $TOKEN") }
+        )
+
+        runServerCall(authInterceptor,
+            { status, _ ->
+                assertThat(status!!.code, equalTo(Status.UNAUTHENTICATED.code))
+            },
+            { _, metadata -> assertThat("Metadata had bearer token.", metadata.containsKey(AUTHORIZATION_METADATA_KEY)) },
+            metadata = Metadata().apply { put(AUTHORIZATION_METADATA_KEY, "Bearer aoeu") }
+        )
     }
 }
---
+++ src/test/kotlin/com/zepben/evolve/conn/grpc/ExceptionInterceptorTest.kt
@@ -0,0 +1,87 @@
+/*
+ * Copyright 2020 Zeppelin Bend Pty Ltd
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+package com.zepben.evolve.conn.grpc
+
+import com.zepben.testutils.auth.MockServerCall
+import com.zepben.testutils.auth.MockServerCallHandler
+import io.grpc.Metadata
+import io.grpc.ServerCall
+import io.grpc.ServerInterceptor
+import io.grpc.Status
+import org.hamcrest.MatcherAssert.assertThat
+import org.hamcrest.Matchers
+import org.hamcrest.Matchers.nullValue
+import org.junit.jupiter.api.Test
+
+class ExceptionInterceptorTest {
+
+    @Test
+    fun `test intercepts exceptions on close and propagates to status`() {
+        // ExceptionInterceptor.close() is what we're testing
+        val interceptor = ExceptionInterceptor()
+
+        runServerCall(interceptor,
+            { status, _ ->
+                assertThat(status!!.code, Matchers.equalTo(Status.INTERNAL.code))
+                assertThat(status.description, Matchers.equalTo("Received no status, this is a bug in the server."))
+            },
+            { call, metadata -> call.close(null, metadata) }
+        )
+
+        runServerCall(interceptor,
+            { status, _ ->
+                assertThat(status!!.code, Matchers.equalTo(Status.OK.code))
+                assertThat(status.description, nullValue())
+            },
+            { call, metadata -> call.close(Status.OK, metadata) }
+        )
+
+        runServerCall(interceptor,
+            { status, _ ->
+                assertThat(status!!.code, Matchers.equalTo(Status.INVALID_ARGUMENT.code))
+                assertThat(status.description, Matchers.equalTo("some description"))
+                assertThat(status.cause, Matchers.instanceOf(IllegalArgumentException::class.java))
+            },
+            { call, metadata -> call.close(Status.UNKNOWN.withCause(IllegalArgumentException("some description")), metadata) }
+        )
+
+        runServerCall(interceptor,
+            { status, _ ->
+                assertThat(status!!.code, Matchers.equalTo(Status.FAILED_PRECONDITION.code))
+                assertThat(status.description, Matchers.equalTo("some description"))
+                assertThat(status.cause, Matchers.instanceOf(IllegalStateException::class.java))
+            },
+            { call, metadata -> call.close(Status.UNKNOWN.withCause(IllegalStateException("some description")), metadata) }
+        )
+
+        runServerCall(interceptor,
+            { status, _ ->
+                assertThat(status!!.code, Matchers.equalTo(Status.UNKNOWN.code))
+                assertThat(status.description, Matchers.equalTo("some description"))
+                assertThat(status.cause, Matchers.instanceOf(NoSuchMethodError::class.java))
+            },
+            { call, metadata -> call.close(Status.UNKNOWN.withCause(NoSuchMethodError("some description")), metadata) }
+        )
+    }
+
+    fun runServerCall(
+        interceptor: ServerInterceptor,
+        sc: (Status?, Metadata?) -> Unit,
+        sch: (ServerCall<Int, Int>, Metadata) -> Unit,
+        metadata: Metadata = Metadata()
+    ) {
+        var wasCalled = false
+        val mockSc = MockServerCall<Int, Int>({ s, m ->
+            sc(s, m)
+            wasCalled = true
+        })
+        interceptor.interceptCall(mockSc, metadata, MockServerCallHandler(sch))
+        assert(wasCalled)
+    }
+}
